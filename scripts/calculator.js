8a8e5ccd9d03418ff6c6f56e0def456f32f84a6e
Teala-B <bradleyteala@gmail.com>
2023-10-23 13:54:07 -0500

    added code and script to calculator

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,43 +1,49 @@
+window.onload = init;
+
+function init()
+{
+    document.getElementById("buttonAdd").onclick = add;
+    document.getElementById("buttonSubtract").onclick = subtract;
+    document.getElementById("buttonMultiply").onclick = multiply;
+    document.getElementById("buttonDivide").onclick = divide;
+}
+
 function add()
 {
-    const number1 = Number(document.getElementById("numer1Field").value);
-    const number2 = Number(document.getElementById("numer2Field").value);
+    const number1 = Number(document.getElementById("number1Field").value);
+    const number2 = Number(document.getElementById("number2Field").value);
 
     const answer = number1 + number2;
-    console.log(answer);
-
+   
     document.getElementById("answerField").value = answer
 }
 
 function subtract()
 {
-    const number1 = Number(document.getElementById("numer1Field").value);
-    const number2 = Number(document.getElementById("numer2Field").value);
+    const number1 = Number(document.getElementById("number1Field").value);
+    const number2 = Number(document.getElementById("number2Field").value);
 
     const answer = number1 - number2;
-    console.log(answer);
-
+    
     document.getElementById("answerField").value = answer
 }
 
 function multiply()
 {
-    const number1 = Number(document.getElementById("numer1Field").value);
-    const number2 = Number(document.getElementById("numer2Field").value);
+    const number1 = Number(document.getElementById("number1Field").value);
+    const number2 = Number(document.getElementById("number2Field").value);
 
     const answer = number1 * number2;
-    console.log(answer);
-
+   
     document.getElementById("answerField").value = answer
 }
 
 function divide()
 {
-    const number1 = Number(document.getElementById("numer1Field").value);
-    const number2 = Number(document.getElementById("numer2Field").value);
+    const number1 = Number(document.getElementById("number1Field").value);
+    const number2 = Number(document.getElementById("number2Field").value);
 
     const answer = number1 / number2;
-    console.log(answer);
 
     document.getElementById("answerField").value = answer
 }
