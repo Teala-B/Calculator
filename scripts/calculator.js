d73bda0077c5e9be998bd04bfcbf13816e461769
Teala-B <bradleyteala@gmail.com>
2023-10-23 13:16:32 -0500

    added calculations for calendar

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,19 +1,43 @@
 function add()
 {
+    const number1 = Number(document.getElementById("numer1Field").value);
+    const number2 = Number(document.getElementById("numer2Field").value);
 
+    const answer = number1 + number2;
+    console.log(answer);
+
+    document.getElementById("answerField").value = answer
 }
 
 function subtract()
 {
+    const number1 = Number(document.getElementById("numer1Field").value);
+    const number2 = Number(document.getElementById("numer2Field").value);
 
+    const answer = number1 - number2;
+    console.log(answer);
+
+    document.getElementById("answerField").value = answer
 }
 
 function multiply()
 {
+    const number1 = Number(document.getElementById("numer1Field").value);
+    const number2 = Number(document.getElementById("numer2Field").value);
 
+    const answer = number1 * number2;
+    console.log(answer);
+
+    document.getElementById("answerField").value = answer
 }
 
 function divide()
 {
+    const number1 = Number(document.getElementById("numer1Field").value);
+    const number2 = Number(document.getElementById("numer2Field").value);
 
+    const answer = number1 / number2;
+    console.log(answer);
+
+    document.getElementById("answerField").value = answer
 }
