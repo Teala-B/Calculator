a8a2f6aa013208e89d3249dd5e02074932cd0b1d
Teala-B <bradleyteala@gmail.com>
2023-10-24 15:30:10 -0500

    commit to be safe

--- scripts/fahrenheit.js
+++ scripts/fahrenheit.js
@@ -1,4 +1,14 @@
+window.onload = init
+function init() {
+    document.getElementById("buttonConvert").onclick = fahrenheitToCelcius
+}
+
 function fahrenheitToCelcius()
 {
+    let fahrenheit = Number(document.getElementById("number1Field").value)
 
+    let answer = (fahrenheit - 32) * 5/9
+
+
+    document.getElementById("answerField").value = answer
 }
